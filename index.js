78627394b30400c285dc474452808a226fa2abcf
Patrickodida <patrick.o.odida@gmail.com>
2024-08-02 15:23:13 +0300

    Update .gitignore and remove tracked files

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import morgan from "morgan";
-const app = express();
-// Middleware to log requests
-app.use(morgan("dev"));
+import path from "path";
+import bodyParser from "body-parser";
 
+const app = express();
+const __dirname = path.resolve();
 // Hardcoded password for Authentication
 const PASSWORD = "passw@rd";
+
+// Middleware to log requests
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Routes
 
 app.get(
   ("/",
@@ -13,6 +21,31 @@ app.get(
     res.send("Welcome to this server application");
   })
 );
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+})
+
+app.post('/login', (req, res) => {
+  const { password } = req.body;
+  if(password === PASSWORD){
+    res.redirect('/node-course.html')
+  } else {
+    res.send('Invalid Password. Please try again!')
+  }
+})
+
+app.get('node-course', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'node-course.html'))
+})
+
+app.use((req, res, next) => {
+  if(req.url === 'node-course'){
+    res.redirect('/login')
+  } else {
+    next()
+  }
+})
 
 const PORT = 4000;
 app.listen(PORT, () => {
