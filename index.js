2bd096dc75d378c7dafdecbb1578eee8574de0f9
Patrickodida <patrick.o.odida@gmail.com>
2024-08-06 15:02:05 +0300

    Protect the (/node-course) route for only authenticated users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import path from "path";
 import bodyParser from "body-parser";
+import session from "express-session";
 
 const app = express();
 const __dirname = path.resolve();
@@ -12,6 +13,24 @@ const PASSWORD = "passw@rd";
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Use session Middleware
+app.use(
+  session({
+    secret: "passw@rd",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Middleware to check whether the user is authenticated
+function isAuthenticated(req, res, next) {
+  if (req.session.isAuthenticated) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
 
 // Routes
 
@@ -26,25 +45,20 @@ app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
+// Set the 'req.session.isAuthenticated' to true upon successful login
 app.post("/login", (req, res) => {
   const { password } = req.body;
   if (password === PASSWORD) {
-    res.redirect("/node-course.html");
+    req.session.isAuthenticated = true;
+    res.redirect("/node-course");
   } else {
     res.send("Invalid Password. Please try again!");
   }
 });
 
-app.get("node-course", (req, res) => {
+// Protect the '/node-course' route for only Authenticated users
+app.get("/node-course", isAuthenticated, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "node-course.html"));
-});
-
-app.use((req, res, next) => {
-  if (req.url === "node-course") {
-    res.redirect("/login");
-  } else {
-    next();
-  }
 });
 
 const PORT = 4000;
